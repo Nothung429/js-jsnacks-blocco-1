f5610d96cc15fe3eaacefb5e7cd435345dfb7cda
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 11:46:51 +0200

    snack-2 inizio

---
+++ snack-2/script.js
@@ -0,0 +1,6 @@
+const firstWord = prompt("Inserisci la prima parola");
+const lengthFirst = firstWord.length;
+console.log(lengthFirst);
+const secondWord = prompt("Inserisci la seconda parola");
+const lengthSecond = secondWord.length;
+console.log(lengthSecond);
