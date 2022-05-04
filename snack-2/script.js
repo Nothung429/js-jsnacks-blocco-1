b7ed603ba20c1a0587890df1138554926fba0284
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 11:50:04 +0200

    snack-2

--- snack-2/script.js
+++ snack-2/script.js
@@ -4,3 +4,10 @@ console.log(lengthFirst);
 const secondWord = prompt("Inserisci la seconda parola");
 const lengthSecond = secondWord.length;
 console.log(lengthSecond);
+if (lengthFirst > lengthSecond) {
+    console.log(firstWord);
+} else if (lengthFirst < lengthSecond) {
+    console.log(secondWord);
+} else {
+    console.log("sono di uguale lunghezza");
+}
