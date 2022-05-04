a9f4c3478bb99362f8f7639f41cbb01e65294ee3
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 18:06:58 +0200

    snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,2 +1,10 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
-
+const numbers = ["1","2","3","4","5","6"];
+const oddNumbers = [];
+console.log(numbers);
+for (let i = 0; i < 6 ; i++) {
+    if (i[numbers] % 2 === 1) {
+        oddNumbers.push(numbers);
+    }
+}
+console.log(oddNumbers);
