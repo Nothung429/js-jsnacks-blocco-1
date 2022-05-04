4fcb215044e4d7a02a847cd7a3258071a9c2429a
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 11:37:01 +0200

    fix

--- snack-1/script.js
+++ snack-1/script.js
@@ -4,8 +4,10 @@ const secondNumber = Number(prompt("Inserisci il secondo numero"));
 console.log(secondNumber);
 if (firstNumber > secondNumber) {
     alert("il primo numero è maggiore del secondo")
+    console.log(firstNumber);
 } else if (firstNumber < secondNumber) {
     alert("il secondo numero è maggiore del primo")
+    console.log(secondNumber);
 } else {
     alert("i due numeri hanno lo stesso valore")
 }
