d73e030941338c697a2d73682f319ab40b78be5c
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 17:39:33 +0200

    correzione snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,8 +1,10 @@
 // Generatore di "nomi cognomi" casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome
 const firstName = ["Mario","Luigi","Giuseppe"];
 const lastName = ["Brambilla","Salvemini","Lopalco"];
-const fullName = [];
-for (let i = 0 ; i < firstName.length ; i++) {
-    fullName.push(firstName[i] + lastName[i]);
+let fullName = [];
+for (let i = 0 ; i < 3 ; i++) {
+    const randomFirstname = Math.floor(Math.random() * firstName.length);
+    const randomLastname = Math.floor(Math.random() * lastName.length);
+    fullName.push(firstName[randomFirstname] + " " + lastName[randomLastname]);
 }
 console.log(fullName);
