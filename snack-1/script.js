4396ec88643c6430dd360619a9f07fef1bc67194
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 11:31:16 +0200

    snack-1 inizio

---
+++ snack-1/script.js
@@ -0,0 +1,4 @@
+const firstNumber = Number(prompt("Inserisci il primo numero"));
+console.log(firstNumber);
+const secondNumber = Number(prompt("Inserisci il secondo numero"));
+console.log(secondNumber);
