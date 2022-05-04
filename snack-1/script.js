1d9ca5cfd95437adde762710f345883b07036588
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 11:33:33 +0200

    snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,10 @@ const firstNumber = Number(prompt("Inserisci il primo numero"));
 console.log(firstNumber);
 const secondNumber = Number(prompt("Inserisci il secondo numero"));
 console.log(secondNumber);
+if (firstNumber > secondNumber) {
+    alert("il primo numero è maggiore del secondo")
+} else if (firstNumber < secondNumber) {
+    alert("il secondo numero è maggiore del primo")
+} else {
+    alert("i due numeri hanno lo stesso valore")
+}
