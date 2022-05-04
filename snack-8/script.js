24fa05e2cc77b00aeb950cab7f6ae69460500201
Nothung429 <frank.gatto96.fg@gmail.com>
2022-05-04 18:39:39 +0200

    risoluzione snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -1,10 +1,10 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
-const numbers = [1,2,3,4,5,6];
+const numbers = [23,78,90,46,67,61];
 console.log(numbers);
-const somma = 0
+let somma = 0
 for (let i = 0; i < numbers.length ; i++) {
-    if( i % 2 === 1 ) {
-        sommaDispari = somma + numbers[i];
+    if (numbers[i] % 2 === 1) {
+        somma = somma + numbers[i];
     }
 }
-console.log(sommaDispari);
+console.log(somma);
